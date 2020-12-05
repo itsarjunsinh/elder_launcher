c948da2cc2f04e2ee6e3ed8cd4680a5c76f276e7
itsarjunsinh <mrarjunsinh@gmail.com>
2020-12-05 14:10:46 +0530

    Update intl translation classes

---
+++ lib/generated/intl/messages_cs.dart
@@ -0,0 +1,46 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a cs locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'cs';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "Apps" : MessageLookupByLibrary.simpleMessage("Aplikace"),
+    "Contacts" : MessageLookupByLibrary.simpleMessage("Kontakty"),
+    "btnAddFavApps" : MessageLookupByLibrary.simpleMessage("Přidat Oblíbené Aplikace"),
+    "btnAddFavContacts" : MessageLookupByLibrary.simpleMessage("Přidat Oblíbené Kontakty"),
+    "btnAllApps" : MessageLookupByLibrary.simpleMessage("Všechny Aplikace"),
+    "btnAllContacts" : MessageLookupByLibrary.simpleMessage("Všechny Kontakty"),
+    "btnBackToHome" : MessageLookupByLibrary.simpleMessage("Zpět Domů"),
+    "btnGrantPermission" : MessageLookupByLibrary.simpleMessage("Udělit Oprávnění"),
+    "dlgAppsAddRemove" : MessageLookupByLibrary.simpleMessage("Přidat/Odebrat Aplikace"),
+    "dlgAppsReload" : MessageLookupByLibrary.simpleMessage("Znovu načíst Seznam Aplikací"),
+    "dlgAppsReorder" : MessageLookupByLibrary.simpleMessage("Přeuspořádat Aplikace"),
+    "dlgCall" : MessageLookupByLibrary.simpleMessage("Volat"),
+    "dlgCancel" : MessageLookupByLibrary.simpleMessage("Zrušit"),
+    "dlgContactsAddRemove" : MessageLookupByLibrary.simpleMessage("Přidat/Odebrat Kontakty"),
+    "dlgContactsReload" : MessageLookupByLibrary.simpleMessage("Znovu načíst Kontakty"),
+    "dlgContactsReorder" : MessageLookupByLibrary.simpleMessage("Přeuspořádat Kontakty"),
+    "dlgEditTitle" : MessageLookupByLibrary.simpleMessage("Změnit Oblíbené"),
+    "msgNoContactsPermission" : MessageLookupByLibrary.simpleMessage("Pro přidání oblíbených kontaktů na tuto stránku povolte této aplikaci přístup ke Kontaktům."),
+    "msgNoData" : MessageLookupByLibrary.simpleMessage("Žádná data k dispozici. Zkuste znova za pár vteřin."),
+    "msgNoFavourites" : MessageLookupByLibrary.simpleMessage("Nepřidali jste žádné oblíbené položky"),
+    "msgNoPhonePermission" : MessageLookupByLibrary.simpleMessage("Pro usnadnění volání, povolte této aplikaci zahájení hovorů.")
+  };
+}
---
+++ lib/generated/intl/messages_pl.dart
@@ -0,0 +1,46 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a pl locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'pl';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "Apps" : MessageLookupByLibrary.simpleMessage("Aplikacje"),
+    "Contacts" : MessageLookupByLibrary.simpleMessage("Kontakty"),
+    "btnAddFavApps" : MessageLookupByLibrary.simpleMessage("Dodaj ulubione aplikacje"),
+    "btnAddFavContacts" : MessageLookupByLibrary.simpleMessage("Dodaj ulubione kontakty"),
+    "btnAllApps" : MessageLookupByLibrary.simpleMessage("Wszystkie Aplikacje"),
+    "btnAllContacts" : MessageLookupByLibrary.simpleMessage("Wszystkie Kontakty"),
+    "btnBackToHome" : MessageLookupByLibrary.simpleMessage("Wrócić do Domu"),
+    "btnGrantPermission" : MessageLookupByLibrary.simpleMessage("Dać Pozwolenie"),
+    "dlgAppsAddRemove" : MessageLookupByLibrary.simpleMessage("Dodaj/Usuń Aplikacje"),
+    "dlgAppsReload" : MessageLookupByLibrary.simpleMessage("Załaduj ponownie listę aplikacji"),
+    "dlgAppsReorder" : MessageLookupByLibrary.simpleMessage("Zmień kolejność aplikacji"),
+    "dlgCall" : MessageLookupByLibrary.simpleMessage("Połączenie"),
+    "dlgCancel" : MessageLookupByLibrary.simpleMessage("Anulować"),
+    "dlgContactsAddRemove" : MessageLookupByLibrary.simpleMessage("Dodaj/Usuń Kontakty"),
+    "dlgContactsReload" : MessageLookupByLibrary.simpleMessage("Załaduj ponownie listę kontaktów"),
+    "dlgContactsReorder" : MessageLookupByLibrary.simpleMessage("Zmień kolejność Kontakty"),
+    "dlgEditTitle" : MessageLookupByLibrary.simpleMessage("Zmień ulubione"),
+    "msgNoContactsPermission" : MessageLookupByLibrary.simpleMessage("Aby dodać ulubione kontakty do tego ekranu, zezwól tej aplikacji na dostęp do swoich kontaktów."),
+    "msgNoData" : MessageLookupByLibrary.simpleMessage("Brak dostępnych danych. Spróbuj ponownie za kilka sekund."),
+    "msgNoFavourites" : MessageLookupByLibrary.simpleMessage("Nie dodałeś żadnych ulubionych."),
+    "msgNoPhonePermission" : MessageLookupByLibrary.simpleMessage("Aby ułatwić dzwonienie, zezwól tej aplikacji na nawiązywanie połączeń telefonicznych za Ciebie.")
+  };
+}
