4eb90c9c6b19ed6f5a8b98792af7cc3eecfc6c7a
itsarjunsinh <mrarjunsinh@gmail.com>
2020-12-19 23:31:01 +0530

    Time and Date based on system locale

--- lib/models/date_time_model.dart
+++ lib/models/date_time_model.dart
@@ -6,10 +6,8 @@ class DateTimeModel extends ChangeNotifier {
   Timer _timer;
   String _date = '';
   String _time = '';
-  String _timeFormat = '';
 
-  DateTimeModel(bool is24HoursFormat) {
-    _timeFormat = is24HoursFormat ? 'HH:MM' : 'h:mm a';
+  DateTimeModel() {
     _timer = Timer.periodic(
         Duration(seconds: 1), (Timer timer) => _updateDateTime());
   }
@@ -24,8 +22,8 @@ class DateTimeModel extends ChangeNotifier {
   }
 
   void _updateDateTime() {
-    _time = DateFormat(_timeFormat).format(DateTime.now());
-    _date = DateFormat('EEEE, MMM d').format(DateTime.now());
+    _time = DateFormat.jm().format(DateTime.now());
+    _date = DateFormat.yMMMMd().format(DateTime.now());
     notifyListeners();
   }
 }
