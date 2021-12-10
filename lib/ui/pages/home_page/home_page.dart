33eccf64f59d514356c0cfc57f9899b6aa10e202
itsarjunsinh <mrarjunsinh@gmail.com>
2021-12-10 19:41:30 +0530

    Remove unused cupertino import
    Use material ExpansionPanel

--- lib/ui/pages/home_page/home_page.dart
+++ lib/ui/pages/home_page/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../constants/edit_mode.dart';
